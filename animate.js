5105f94d1ad589bad6fc4de20252190b82242cdb
c-sk8 <cskent8@gmail.com>
2026-05-13 19:48:06 +0100

    New function for controlling rotation

--- animate.js
+++ animate.js
@@ -9,21 +9,25 @@ import * as THREE from './three.module.js';
 import { scene } from './scene.js';
 import { mesh } from './surface-builder.js';
 
-let rotationXVelocity = 0;
-let rotationYVelocity = 0;
-let rotationZVelocity = 0;
-let targetXVelocity = 0.12;
-let targetYVelocity = 0.15;
-let targetZVelocity = 0;
+let x_rotation_velocity = 0;
+let y_rotation_velocity = 0;
+let z_rotation_velocity = 0;
 
-let cameraZ = 5;
+let x_target_rotation_velocity = 0;
+let y_target_rotation_velocity = 0;
+let z_target_rotation_velocity = 0;
+
+const VELOCITY_SCALE = 0.1;
+const MOTION_DAMPENER = 0.06;
+
+let cameraZ = 3;
 let targetCameraZ = 3;
 const step = 0.5;
 
 let clock = new THREE.Clock();
 
 const camera = new THREE.PerspectiveCamera(50, innerWidth / innerHeight, 0.1, 100);
-camera.position.set(0, 1, 2.5);
+camera.position.set(0, 0, 2.5);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -34,9 +38,45 @@ export function zoomIn() { targetCameraZ -= step; }
 export function zoomOut() { targetCameraZ += step; }
 
 export function resetRotationVelocity() {
-	rotationXVelocity = 0;
-	rotationYVelocity = 0;
-	rotationZVelocity = 0;
+	x_rotation_velocity = 0;
+	y_rotation_velocity = 0;
+	z_rotation_velocity = 0;
+}
+
+export function set_X_RotationVelocity(velocity) {
+	x_rotation_velocity = velocity;
+}
+
+export function set_Y_RotationVelocity(velocity) {
+	y_rotation_velocity = velocity;
+}
+
+export function isUpsideDown() {
+	return (Math.cos(mesh.rotation.x) < 0);
+}
+
+export function set_X_TargetRotationVelocity(target_velocity) {
+	x_target_rotation_velocity = target_velocity;
+}
+
+export function get_X_TargetRotationVelocity() {
+	return x_target_rotation_velocity;
+}
+
+export function set_Y_TargetRotationVelocity(target_velocity) {
+	y_target_rotation_velocity = target_velocity;
+}
+
+export function get_Y_TargetRotationVelocity() {
+	return y_target_rotation_velocity;
+}
+
+export function set_Z_TargetRotationVelocity(target_velocity) {
+	z_target_rotation_velocity = target_velocity;
+		}
+
+export function get_Z_TargetRotationVelocity() {
+	return z_target_rotation_velocity;
 }
 
 export function animate() {
@@ -44,14 +84,14 @@ export function animate() {
     const delta = clock.getDelta();
 
     // --- Rotation ---
-	rotationXVelocity += (targetXVelocity - rotationXVelocity) * 0.02;
-	mesh.rotation.x += rotationXVelocity * delta;
+	x_rotation_velocity += (x_target_rotation_velocity - x_rotation_velocity) * MOTION_DAMPENER;
+	mesh.rotation.x += x_rotation_velocity * VELOCITY_SCALE * delta;
 	
-	rotationYVelocity += (targetYVelocity - rotationYVelocity) * 0.02;
-	mesh.rotation.y += rotationYVelocity * delta;
+	y_rotation_velocity += (y_target_rotation_velocity - y_rotation_velocity) * MOTION_DAMPENER;
+	mesh.rotation.y += y_rotation_velocity * VELOCITY_SCALE * delta;
 	
-	rotationZVelocity += (targetZVelocity - rotationZVelocity) * 0.02;
-	mesh.rotation.z += rotationZVelocity * delta;
+	z_rotation_velocity += (z_target_rotation_velocity - z_rotation_velocity) * MOTION_DAMPENER;
+	mesh.rotation.z += z_rotation_velocity * VELOCITY_SCALE * delta;
 
 	cameraZ += (targetCameraZ - cameraZ) * 0.1;
 	camera.position.z = cameraZ;
